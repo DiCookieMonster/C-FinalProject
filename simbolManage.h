6c52881f195dd4ed1045decca1dcdd3d6abb3267
DiCookieMonster <dina.suranovich@gmail.com>
2014-11-03 00:22:08 +0800

    Create simbolManage.h

---
+++ simbolManage.h
@@ -0,0 +1,15 @@
+/*
+ =============================================================
+ program    : final project Systems Programming Laboratory (20465)
+ Author     : Dina Suranovich
+ =============================================================
+ */
+ 
+
+#include "symbol.h"
+
+Symbol* GetSymbol (char* name);
+int AddSymbol(char* name, int type, int isExtern, int isEntry, int address);
+int CheckLabelNameLegality (char *label);
+
+
