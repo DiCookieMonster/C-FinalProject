681ca3830da22d6f31ce197893fcd2f8e39adaff
DiCookieMonster <dina.suranovich@gmail.com>
2014-11-03 00:11:54 +0800

    Create data.c

---
+++ data.c
@@ -0,0 +1,13 @@
+/*
+ =============================================================
+ program    : final project Systems Programming Laboratory (20465)
+ Author     : Dina Suranovich
+ =============================================================
+ */
+ 
+#include <stdio.h>
+#include "global.h"
+
+int DC;
+int IC;
+int DataArray[MAX_ARRAY_LENGTH];
