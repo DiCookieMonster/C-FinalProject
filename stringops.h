9f5246e89989dc78b1e5ab5ffcbe2d02be5792fd
DiCookieMonster <dina.suranovich@gmail.com>
2014-11-03 00:20:46 +0800

    Create stringops.h

---
+++ stringops.h
@@ -0,0 +1,15 @@
+/*
+ =============================================================
+ program    : final project Systems Programming Laboratory (20465)
+ Author     : Dina Suranovich
+ =============================================================
+ */
+ 
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "global.h"
+
+
+char* strtokEx (char* str, char* seps, int index);
+
