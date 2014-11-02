3d55d643160007dee5e6e31fef5474477c33b395
DiCookieMonster <dina.suranovich@gmail.com>
2014-11-03 00:12:54 +0800

    Create dispose.h

---
+++ dispose.h
@@ -0,0 +1,16 @@
+/*
+ =============================================================
+ program    : final project Systems Programming Laboratory (20465)
+ Author     : Dina Suranovich
+ =============================================================
+
+ */
+ 
+void FreeSymbols ();
+
+extern int DC;
+extern int IC;
+extern int FIC;
+extern int countLine;
+extern int FirstPhaseSuccess;
+extern int SeconnedPhaseSucess;
