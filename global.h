05b0fc73ca3ff75dbe5d991b2caf036724a06b14
DiCookieMonster <dina.suranovich@gmail.com>
2014-11-03 00:15:17 +0800

    Create global.h

---
+++ global.h
@@ -0,0 +1,15 @@
+/*
+=============================================================
+program : final project Systems Programming Laboratory (20465)
+Author : Dina Suranovich
+=============================================================
+*/
+int success;
+#define MAX_INSTRUCTION 17
+#define MAX_LABEL 30
+#define MAX_LINE 80
+#define MAX_ARRAY_LENGTH 2000
+#define MAX_REGISTER 8
+#define MAX_FILE_NAME 20
+#define START_ADDRESS 100
+#define MAX_STRING_LENGTH 15
